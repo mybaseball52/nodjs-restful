6f735963c80e6348e0630a47cad15cc963cd577d
mybaseball52 <mybaseball52@gmail.com>
2017-07-28 12:37:28 +0800

    clean up codes and implement put/patch method call

--- app.js
+++ app.js
@@ -1,56 +1,26 @@
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 var db = mongoose.connect('mongodb://demo:123456789@ds050879.mlab.com:50879/standupmeetingnotes');
 
 var Book = require('./models/bookModel');
+// var Author = require('./models/authorModel');
 
 var app = express();
-
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/Books')
-	.get(function(req,res){
-		var query = {};
+bookRouter = require('./routes/bookRoutes')(Book);
+// authorRouter = require('./routes/authorRouter')(Author);
 
-		if(req.query.genre){
-			query.genre = req.query.genre;
-		}
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
 
-		Book.find(query, function(err, books){
-			if(err)
-				res.status(500).send(err);
-			else
-				res.json(books);
-		});
-	});
-
-bookRouter.route('/Books/:bookId')
-	.get(function(req,res){
-		var query = {};
-
-		if(req.query.genre){
-			query.genre = req.query.genre;
-		}
-		
-		// get item by parameters 
-		Book.find(req.params.bookId, function(err, books){
-			if(err)
-				res.status(500).send(err);
-			else
-				res.json(books);
-		});
-	});
-
-app.use('/api', bookRouter);
-
-app.use('/', function(req,res){
+app.get('/', function(req,res){
 	res.send('Welcome to my API!');
 });
-
-
 
 
 app.listen(port, function(){
